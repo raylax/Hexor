f29ad5dd858ebfe4a5f7b836675c3c869a710a9d
raylax <raylax@inurl.org>
2023-04-13 15:18:31 +0800

    优化性能

--- src/components/editor/Data.tsx
+++ src/components/editor/Data.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {memo} from 'react'
 import {makeStyles, tokens} from '@fluentui/react-components'
 import {toAscii, toHex} from '@/utils'
 import {DataRange} from '@/components/editor/types'
 import {useSelectData} from '@/components/editor/hooks'
-import {FixedSizeList as List, ListChildComponentProps} from 'react-window'
+import {FixedSizeList as List, ListChildComponentProps, areEqual} from 'react-window'
 import AutoSizer from "react-virtualized-auto-sizer"
 import {FileBuffer} from '@/file'
 import clsx from 'clsx'
+import memoize from "memoize-one"
+import * as buffer from 'buffer'
 
 interface Props {
   buffer: FileBuffer
@@ -49,65 +51,74 @@ const useStyles = makeStyles({
   },
 })
 
-const Data: React.FC<Props> = (props) => {
+type DataRowProps = { props: Props, s: ReturnType<typeof useSelectData> }
 
+const DataRow = memo(({index, style, data: { props, s }}: ListChildComponentProps<DataRowProps>) => {
 
   const calculateAddress = (row: number, col: number) => row * props.bytesPerRow + col
+
   const styles = useStyles()
-  const { isSelected, handleMouseDown, handleMouseMove, handleMouseUp } = useSelectData(props.bytesPerRow, props.selected, props.onSelect)
 
-  const renderRow = ({index, isScrolling, style}: ListChildComponentProps) => {
-
-
-    return (
-      <div
-        className={styles.row}
-        style={style}
-      >
-
-        <div>
+  return (
+    <div
+      key={index}
+      className={styles.row}
+      style={style}
+    >
+      <div>
           <span
             className={`${styles.item} ${styles.address}`}
           >
             {toHex(index * props.bytesPerRow, 8)}
           </span>
-        </div>
+      </div>
 
-        <div onMouseUp={handleMouseUp}>
-          {[...Array(props.bytesPerRow)]
-            .map((_, col) => ({ col, address: calculateAddress(index, col) }))
-            .map(({ col, address }) => (
-              <span
-                className={isSelected(address) ? clsx(styles.item, styles.byteSelected) : styles.item}
-                key={col}
-                onMouseDown={(e) => handleMouseDown(e, address)}
-                onMouseMove={() => handleMouseMove(address)}
-              >
+      <div onMouseUp={s.handleMouseUp}>
+        {[...Array(props.bytesPerRow)]
+          .map((_, col) => ({ col, address: calculateAddress(index, col) }))
+          .map(({ col, address }) => (
+            <span
+              className={s.isSelected(address) ? clsx(styles.item, styles.byteSelected) : styles.item}
+              key={col}
+              onMouseDown={(e) => s.handleMouseDown(e, address)}
+              onMouseMove={() => s.handleMouseMove(address)}
+            >
                 {toHex(props.buffer.read(address), 2)}
               </span>
-            ))
-          }
-        </div>
+          ))
+        }
+      </div>
 
-        <div onMouseUp={handleMouseUp}>
-          {[...Array(props.bytesPerRow)]
-            .map((_, col) => ({ col, address: calculateAddress(index, col) }))
-            .map(({ col, address }) => (
-              <span
-                key={col}
-                className={isSelected(address) ? clsx(styles.item, styles.text, styles.textSelected) : `${styles.item} ${styles.text}`}
-                onMouseDown={(e) => handleMouseDown(e, address)}
-                onMouseMove={() => handleMouseMove(address)}
-              >
+      <div onMouseUp={s.handleMouseUp}>
+        {[...Array(props.bytesPerRow)]
+          .map((_, col) => ({ col, address: calculateAddress(index, col) }))
+          .map(({ col, address }) => (
+            <span
+              key={col}
+              className={s.isSelected(address) ? clsx(styles.item, styles.text, styles.textSelected) : `${styles.item} ${styles.text}`}
+              onMouseDown={(e) => s.handleMouseDown(e, address)}
+              onMouseMove={() => s.handleMouseMove(address)}
+            >
                 {toAscii(props.buffer.read(address))}
               </span>
-            ))
-          }
-        </div>
-
+          ))
+        }
       </div>
-    )
-  }
+
+    </div>
+  )
+}, areEqual)
+
+const createData = memoize((props, s) => ({
+  props: props,
+  s: s,
+}))
+
+const Data: React.FC<Props> = (props) => {
+
+  const styles = useStyles()
+  const s = useSelectData(props.bytesPerRow, props.selected, props.onSelect)
+  const itemData = createData(props, s)
 
   return (
     <div className={styles.root}>
@@ -116,11 +127,12 @@ const Data: React.FC<Props> = (props) => {
           <List
             itemCount={props.rows}
             itemSize={22}
+            itemData={itemData}
             width={width || 0}
             height={height || 0}
             overscanCount={30}
           >
-            {renderRow}
+            {DataRow}
           </List>
         )}
       </AutoSizer>
